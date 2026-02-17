a7a138d5654beba57e1ad8ad5619e90e7be66af8
JOYAL ANTONY <155997915+joyalantony321@users.noreply.github.com>
2026-02-17 17:59:25 +0400

    ll

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/kanban":["static\u002Fchunks\u002Fpages\u002Fkanban.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fkanban"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
